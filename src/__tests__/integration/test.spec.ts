1f3c77572b747f72e223e7a437b39da3c7d8595e
Josh Wulf <josh@magikcraft.io>
2019-03-25 17:09:21 +1000

    Fix failing integration test

--- src/__tests__/integration/test.spec.ts
+++ src/__tests__/integration/test.spec.ts
@@ -5,6 +5,6 @@ describe('ZBClient', () => {
 	it('deploys a workflow', async () => {
 		const zbc = new ZBClient('localhost')
 		const result = await zbc.deployWorkflow(`${__dirname}/test.bpmn`)
-		expect(result.workflows[0].bpmnProcessId).toBe('hello-world')
+		expect(result.workflows[0].bpmnProcessId).toBe('test-process')
 	})
 })
