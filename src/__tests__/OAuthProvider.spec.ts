1973956b44f12515c76823f6e03160bef2bb0df9
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2024-02-29 16:03:09 +0100

    test: rewrite test cleanup
    Ensures we properly clean up after each test,
    also in cases of test failures.

--- src/__tests__/OAuthProvider.spec.ts
+++ src/__tests__/OAuthProvider.spec.ts
@@ -6,6 +6,8 @@ import { OAuthProvider } from '../lib/OAuthProvider'
 const STORED_ENV = {}
 const ENV_VARS_TO_STORE = ['ZEEBE_TOKEN_CACHE_DIR']
 
+const tokenCache = path.join(__dirname, '.token-cache');
+
 beforeAll(() => {
 	ENV_VARS_TO_STORE.forEach(e => {
 		STORED_ENV[e] = process.env[e]
@@ -13,7 +15,11 @@ beforeAll(() => {
 	})
 })
 
-afterAll(() => {
+afterEach(() => {
+	clearCache(tokenCache);
+});
+
+afterEach(() => {
 	ENV_VARS_TO_STORE.forEach(e => {
 		delete process.env[e]
 		if (STORED_ENV[e]) {
@@ -23,12 +29,6 @@ afterAll(() => {
 })
 
 test("Creates the token cache dir if it doesn't exist", () => {
-	const tokenCache = path.join(__dirname, '.token-cache')
-	if (fs.existsSync(tokenCache)) {
-		fs.rmdirSync(tokenCache)
-	}
-	expect(fs.existsSync(tokenCache)).toBe(false)
-
 	const o = new OAuthProvider({
 		audience: 'token',
 		cacheDir: tokenCache,
@@ -39,19 +39,10 @@ test("Creates the token cache dir if it doesn't exist", () => {
 	})
 	expect(o).toBeTruthy()
 	expect(fs.existsSync(tokenCache)).toBe(true)
-	if (fs.existsSync(tokenCache)) {
-		fs.rmdirSync(tokenCache)
-	}
-	expect(fs.existsSync(tokenCache)).toBe(false)
 	o.stopExpiryTimer()
 })
 
 test('Gets the token cache dir from the environment', () => {
-	const tokenCache = path.join(__dirname, '.token-cache')
-	if (fs.existsSync(tokenCache)) {
-		fs.rmdirSync(tokenCache)
-	}
-	expect(fs.existsSync(tokenCache)).toBe(false)
 	process.env.ZEEBE_TOKEN_CACHE_DIR = tokenCache
 	const o = new OAuthProvider({
 		audience: 'token',
@@ -62,49 +53,27 @@ test('Gets the token cache dir from the environment', () => {
 	})
 	expect(o).toBeTruthy()
 	expect(fs.existsSync(tokenCache)).toBe(true)
-	if (fs.existsSync(tokenCache)) {
-		fs.rmdirSync(tokenCache)
-	}
-	expect(fs.existsSync(tokenCache)).toBe(false)
 	o.stopExpiryTimer()
 })
 
 test('Uses an explicit token cache over the environment', () => {
-	const tokenCache1 = path.join(__dirname, '.token-cache1')
-	const tokenCache2 = path.join(__dirname, '.token-cache2')
-	;[tokenCache1, tokenCache2].forEach(tokenCache => {
-		if (fs.existsSync(tokenCache)) {
-			fs.rmdirSync(tokenCache)
-		}
-		expect(fs.existsSync(tokenCache)).toBe(false)
-	})
-	process.env.ZEEBE_TOKEN_CACHE_DIR = tokenCache1
+	const tokenCache_other = path.join(__dirname, '.token-cache2')
+	process.env.ZEEBE_TOKEN_CACHE_DIR = tokenCache_other
 	const o = new OAuthProvider({
 		audience: 'token',
-		cacheDir: tokenCache2,
+		cacheDir: tokenCache,
 		cacheOnDisk: true,
 		clientId: 'clientId',
 		clientSecret: 'clientSecret',
 		url: 'url',
 	})
 	expect(o).toBeTruthy()
-	expect(fs.existsSync(tokenCache2)).toBe(true)
-	expect(fs.existsSync(tokenCache1)).toBe(false)
-	;[tokenCache1, tokenCache2].forEach(tokenCache => {
-		if (fs.existsSync(tokenCache)) {
-			fs.rmdirSync(tokenCache)
-		}
-		expect(fs.existsSync(tokenCache)).toBe(false)
-	})
+	expect(fs.existsSync(tokenCache)).toBe(true)
+	expect(fs.existsSync(tokenCache_other)).toBe(false)
 	o.stopExpiryTimer()
 })
 
 test('Throws in the constructor if the token cache is not writable', () => {
-	const tokenCache = path.join(__dirname, '.token-cache')
-	if (fs.existsSync(tokenCache)) {
-		fs.rmdirSync(tokenCache)
-	}
-	expect(fs.existsSync(tokenCache)).toBe(false)
 	fs.mkdirSync(tokenCache, 0o400)
 	expect(fs.existsSync(tokenCache)).toBe(true)
 	let thrown = false
@@ -124,10 +93,6 @@ test('Throws in the constructor if the token cache is not writable', () => {
 		thrown = true
 	}
 	expect(thrown).toBe(true)
-	if (fs.existsSync(tokenCache)) {
-		fs.rmdirSync(tokenCache)
-	}
-	expect(fs.existsSync(tokenCache)).toBe(false)
 })
 
 test('Send form encoded request', () => {
@@ -281,3 +246,11 @@ test('In-memory cache is populated and evicted after timeout', () => {
 			return server.close()
 		})
 })
+
+
+function clearCache(cachePath) {
+	if (fs.existsSync(cachePath)) {
+		fs.rmSync(cachePath, { recursive: true })
+	}
+	expect(fs.existsSync(cachePath)).toBe(false)
+}
